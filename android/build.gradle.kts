1796c63742d5c160722231e2b2fffa10b80cf3c6
xgg240 <2600481@qq.com>
2026-06-04 02:30:54 +0800

    fix: force compileSdk 36 for all subprojects (plugins)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,6 +3,16 @@ allprojects {
         google()
         mavenCentral()
     }
+
+    // Force compileSdk 36 for all subprojects (plugins)
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val androidExt = project.extensions.findByName("android")
+            if (androidExt is com.android.build.gradle.BaseExtension) {
+                androidExt.compileSdkVersion(36)
+            }
+        }
+    }
 }
 
 val newBuildDir: Directory =
